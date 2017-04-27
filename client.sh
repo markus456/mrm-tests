980e52d9f9fedc808a9570c847f4ebf871d375e7
Markus Mäkelä <markus456@gmail.com>
2017-04-27 11:15:33 +0300

    Add simple test client
    The client creates an insert load on the databases by doing inserts
    through MaxScale.

---
+++ client.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# This is a simple client that creates a table and inserts data into it
+# every second
+#
+
+user=skysql
+password=skysql
+host=192.168.121.141
+port=4006
+
+table_def="CREATE OR REPLACE TABLE test.t1 (id INT)"
+table_insert="INSERT INTO test.t1 VALUES (1)"
+
+mysql -u $user -p$password -h $host -P $port -e "$table_def"
+
+
+while true
+do
+    mysql -u $user -p$password -h $host -P $port -e "$table_def"
+    sleep 1
+done
